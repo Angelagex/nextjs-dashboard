f802b60891634499e8fd36c926dc9d7b8110834e
Angel Agelvis <agelvisangel@gmail.com>
2023-12-12 12:23:35 -0500

    ADD - Dashboard, DB settings, Sidebar

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { fetchRevenue } from "../lib/data"
+
+export default async function Dashboard() {
+    const revenue = await fetchRevenue()
+    console.log(revenue)
+    return <div>Dashboard</div>
+}
