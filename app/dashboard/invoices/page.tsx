3d563ba77c75ca7bd757efce47bb0232dcda420d
Angel Agelvis <agelvisangel@gmail.com>
2023-12-21 11:37:05 -0500

    ADD - Invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 interface SearchParams {
     searchParams: {
         query: string;
@@ -16,6 +17,7 @@ interface SearchParams {
 export default async function Invoices({searchParams} : SearchParams) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.currentPage) || 1;
+    const totalPages = await fetchInvoicesPages(query);
 
     return (
         <div className="w-full">
@@ -30,7 +32,7 @@ export default async function Invoices({searchParams} : SearchParams) {
                 <Table query={query} currentPage={currentPage} />
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     );
